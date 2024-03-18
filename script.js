15f6ddef6ef0f9c68474c8f04fb3eafaff69d841
Hazel0404 <162906307+Hazel0404@users.noreply.github.com>
2024-03-18 15:48:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("randomPickButton").addEventListener("click", function() {
-    const names = ['马驰恒', '高添铄', '金沫函', '岳柏帆', '尹怡丁', '杨翼帆', '张兆慷', '刘潇蔚', '田悦然', '郑寒月', '申雪', '陈轩宇', '刘雅', '倪子贺', '和芊辰', '李思达', '王优优', '陈熙薷', '杨博铄', '宋紫嫚', '张馨宇', '张润一', '岳佳彤', '侯文鑫', '刘梦洁', '孙颢宸', '赵淑颖', '胡竞文', '丁焱鑫'];
+    const names = ['田晞灿', '武子博', '张奥', '汤诚', '王相然', '高森', '李秉昊', '杨开程', '王程轩', '杜庚轩', '田宸赫', '李抒菲', '韩铠航', '宋祥宁', '田潇悦', '陈柯羽', '高篪', '任星宇'];
     const pickedName = names[Math.floor(Math.random() * names.length)];
     document.getElementById("pickedName").textContent = `被点到的人是：${pickedName}`;
 });
